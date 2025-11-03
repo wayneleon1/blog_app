2f8d8b6565166b8b6eec5188a86246a9d5c0f81f
wayneleon1 <rurangwaleon10@gmail.com>
2025-11-03 16:44:03 +0200

    connected supabae to the project

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = "https://rmwndowrudmlmaznytgt.supabase.co";
+  static const supabaseAnonKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtd25kb3dydWRtbG1hem55dGd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxNjc2MTcsImV4cCI6MjA3Nzc0MzYxN30.04qxToPt4lPNxDJKZXj5AuDlXpIAUYkXWoiEp-5gRks";
+}
