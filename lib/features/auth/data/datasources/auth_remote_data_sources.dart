70c95a3636f7f3278f85b06b1fcc20f74b2ee3b9
wayneleon1 <rurangwaleon10@gmail.com>
2025-11-03 17:13:43 +0200

    Added auth remote data source for signup

---
+++ lib/features/auth/data/datasources/auth_remote_data_sources.dart
@@ -0,0 +1,48 @@
+import 'package:blog_app/core/error/exceptions.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDataSources {
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRemoteDataSourcesImpl implements AuthRemoteDataSources {
+  final SupabaseClient supabaseClient;
+  AuthRemoteDataSourcesImpl(this.supabaseClient);
+  @override
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        password: password,
+        email: email,
+        data: {'name': name},
+      );
+
+      if (response.user == null) {
+        throw const ServerException('User is null');
+      }
+      return response.user!.id;
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+}
